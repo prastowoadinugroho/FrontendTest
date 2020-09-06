18a6cb936cc880f2b7a02a9fcff0fd82c9813af5
PrastowoAdi <prastowoadinugroho@gmail.com>
2020-09-06 13:40:42 +0700

    update-file

--- js/main.js
+++ js/main.js
@@ -6,9 +6,9 @@ var arrTextBtn = ["Approved", "In Progress", "In Riview", "Waiting"];
 var arrColors = ['#C9FDFB', '#D2EFFF', '#F5D2C2'];
 
 function addItem(){
-    var list = document.getElementById("text-list");
-    var checkllist = document.getElementById("checkllist");
-    var button = document.getElementById("button");
+    var list = document.getElementById("div1");
+    var checkllist = document.getElementById("div1");
+    var button = document.getElementById("div1");
     var textList = document.getElementById("textList");
     var imgRand =  document.getElementById("imgRand");
     var random_color = arrColors[Math.floor(Math.random() * arrColors.length)];
@@ -17,8 +17,8 @@ function addItem(){
     randomImage = arrImg[Math.floor(Math.random() * arrImg.length)];
     randomBtnText = arrTextBtn[Math.floor(Math.random() * arrTextBtn.length)];
 
-    var listTask = document.createElement("div");
     var img = document.createElement("img");
+    var listTask = document.createElement("p");
     
     var btn = document.createElement("button");
     btn.innerHTML = randomBtnText;
@@ -26,7 +26,8 @@ function addItem(){
     listTask.setAttribute('id',textList.value = randomText);
     img.setAttribute("src", imgRand.value =randomImage);
     listTask.appendChild (document.createTextNode(textList.value));
-    list.appendChild(listTask);
+    
     checkllist.appendChild(img);
+    list.appendChild(listTask);
     button.appendChild(btn).style.backgroundColor=random_color;
 }
